5fbb03670adde53883e38aa7725fb5f8bfb76a14
suecher <suecher@qq.com>
2016-04-08 01:29:39 +0800

    init

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ let app = express();
 
 app.route('/')
     .get(function(req,res){
+
+        console.log(req.host);
+
         res.sendfile('./public/index.html');
+
     });
 
-app.listen(3011,'www.kit.cool',function(){
+app.listen(3011,function(){
     console.log('strat listen 3011');
 });
